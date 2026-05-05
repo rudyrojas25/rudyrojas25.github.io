4c54edcea2a842b79cc76cf4a20c04e5a3e02ce3
rudyrojas25 <ruarojas@ucsc.edu>
2026-05-05 00:04:32 -0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,30 @@
+// Jim Whitehead
+// Created: 4/14/2024
+// Phaser: 3.70.0
+//
+// Cubey
+//
+// An example of putting sprites on the screen using Phaser
+// 
+// Art assets from Kenny Assets "Shape Characters" set:
+// https://kenney.nl/assets/shape-characters
+
+// debug with extreme prejudice
+"use strict"
+
+// game config
+let config = {
+    parent: 'phaser-game',
+    type: Phaser.CANVAS,
+    render: {
+        pixelArt: true  // prevent pixel art from getting blurred when scaled
+    },
+    width: 1200, //og 800
+    height: 600, //og 600
+    scene: [OneD] //og OneD
+}
+
+//convenience object that contains all sprites
+let my = {sprite: {}};
+
+const game = new Phaser.Game(config);
